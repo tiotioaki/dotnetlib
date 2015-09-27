9ffbb9f315037c736712b97e4efbffa11a88cb00
tiotioaki <tiotio.aki15@gmail.com>
2015-09-27 21:42:04 +0900

    Add Files

---
+++ dotnetlib/Tiotioaki.Data/src/BaseDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiotioaki.Data
+{
+    public class BaseDataAccess : BaseClass
+    {
+        public BaseDataAccess()
+        {
+
+        }
+    }
+}
